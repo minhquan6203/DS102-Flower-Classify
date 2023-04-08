4bad4bb663c022f2a4c8893b159c76b82ce431dd
quan nguyen <21521333@gm.uit.edu.vn>
2023-04-08 13:53:59 +0700

    fix model.py

--- model.py
+++ model.py
@@ -8,7 +8,7 @@ class CNN_Model(nn.Module): #this repo use ResNet34
     def __init__(self, config):
         super(CNN_Model, self).__init__()
         self.num_classes = config.num_classes
-        self.resnet = models.resnet34(pretrained=False)
+        self.resnet = models.resnet34(pretrained=True)
         self.resnet.fc = nn.Linear(self.resnet.fc.in_features, self.num_classes)
 
     def forward(self, x):
