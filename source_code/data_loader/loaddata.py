59fdb052fca93c44d5d86b9ff8739e06c0f0458f
quan nguyen <21521333@gm.uit.edu.vn>
2023-05-11 12:24:46 +0700

    fix kmeans

--- source_code/data_loader/loaddata.py
+++ source_code/data_loader/loaddata.py
@@ -30,7 +30,6 @@ class LoadData:
             transform = transforms.Compose([
                 transforms.Resize((self.image_H, self.image_W)),
                 transforms.ToTensor(),
-                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
             ])
              
 
